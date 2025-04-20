96f0766f852878c3e8166c331784a90cc41f66bb
hayamouneimne <162468015+hayamouneimne@users.noreply.github.com>
2025-04-20 14:50:00 +0300

    Update businessProfile.js
    small fixes

--- businessProfile.js
+++ businessProfile.js
@@ -23,7 +23,7 @@ export function eventList() {
     const postMedia = document.getElementById('postMedia');
     const postList = document.getElementById('postList');
     editButton(editBtn, editNameInput, profilePicInput, pfpMain, nameFr, editBox, nameLimit);
-    saveButton(saveBtn, editNameInput, editMissionInput, nameFr, mission, pfpMain, sidebarPfp, editBox);
+    saveButton(saveBtn, editNameInput, number, nameFr, pfpMain, sidebarPfp, editBox, editNumberInput);
     exitButton(closeEB, editBox);
     changes(fileInput, profilePicInput);
     postRelated(addPostBtn, postModal, closePostModal);
@@ -43,7 +43,7 @@ export function editButton(editBtn, editNameInput, profilePicInput, pfpMain, nam
   });
 }
 
-export function saveButton(saveBtn, editNameInput, editMissionInput, nameFr, mission, pfpMain, sidebarPfp, editBox) {
+export function saveButton(saveBtn, editNameInput, number, nameFr, pfpMain, sidebarPfp, editBox, editNumberInput) {
   saveBtn.addEventListener('click', () => {
     const newName = editNameInput.value.trim();
     const newNumber = editNumberInput.value.trim();
